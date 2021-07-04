6ccd0d2edfa8f3c6da61d88d963713f4da479e17
ChampionsofEurope <86567033+ChampionsofEurope@users.noreply.github.com>
2021-07-04 17:53:29 +0100

    Create Functions_IN_LIST.py

---
+++ Functions_IN_LIST.py
@@ -0,0 +1,18 @@
+/4.7.21
+
+p = ["CHELSEA","MAN U","MAN CITY","SPURS","ARSENAL","LIVERPOOL","PSG"]
+print(p)
+
+#Only printing 1
+k = p[1]
+print(k)
+
+#Deleting from list
+print(p[4])
+
+#Adding to list
+p[2] = "LILLE"
+print(p)
+
+#Reversing Deleting
+print(p[-1])
