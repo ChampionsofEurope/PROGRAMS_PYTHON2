303f1cb115f425957e7a8c8f522b5a87b3dfd044
ChampionsofEurope <86567033+ChampionsofEurope@users.noreply.github.com>
2021-08-27 16:54:52 +0100

    Create Password.py

---
+++ Password.py
@@ -0,0 +1,11 @@
+Pass = input("Enter Password")
+
+inty = 1,2,3,4,5,6,7,8,9,0
+
+
+if Pass.isupper() == 0:
+    print("Correct")
+elif Pass == inty:
+    print("Correct")
+else:
+    print("Incorrect")
